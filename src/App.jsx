9946d4a034b0d06213bc92f5a0c87449ca07a655
sumch76 <sumitchaubey76@gmail.com>
2024-12-31 01:36:03 +0530

    added redux toolkit till now and created a utils folder where is our appStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Body from "./components/Body";
 import Login from "./components/Login";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 function App() {
 
   return (
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename="/">
     <Routes>
       <Route path="/" element={<Body />} />
       <Route path="/login" element={<Login />} />
     </Routes>
     </BrowserRouter>
-     
+    </Provider>
+   
+      
     </>
   )
 }
